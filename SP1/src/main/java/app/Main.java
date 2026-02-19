b7b8997b92ff6cd9614df47b7de5db23a50c503a
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-02-19 10:41:52 +0100

    First commit + Pom.xml

---
+++ SP1/src/main/java/app/Main.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Main {
+
+    // Attributes
+
+    // ______________________________________________
+
+    public static void main(String[] args) {
+
+    //TIP Hej venner!
+
+    }
+
+}
