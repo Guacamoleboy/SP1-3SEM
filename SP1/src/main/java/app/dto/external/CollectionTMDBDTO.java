d5146a6ae946b8d7e7ab8911a3c9d579913a1ba5
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-02-23 22:34:09 +0100

    Issue #45

---
+++ SP1/src/main/java/app/dto/external/CollectionTMDBDTO.java
@@ -0,0 +1,47 @@
+package app.dto.external;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CollectionTMDBDTO {
+
+    // Endpoint for
+    // ____________
+    // https://api.themoviedb.org/3/collection/{collection_id}
+
+    // Attributes
+    private Long id;
+    private String name;
+    @JsonProperty("original_language")
+    private String originalLanguage;
+    @JsonProperty("original_name")
+    private String originalName;
+    private String overview;
+    @JsonProperty("poster_path")
+    private String posterPath;
+    @JsonProperty("backdrop_path")
+    private String backdropPath;
+    private List<Part> parts;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Part {
+        private Long id;
+        private String name;
+    }
+
+}
