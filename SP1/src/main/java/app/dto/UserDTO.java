0813aaeb8f1a1756d00399d3d8a8689bfe0f0307
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2026-02-24 09:42:23 +0100

    UserDTO

--- SP1/src/main/java/app/dto/UserDTO.java
+++ SP1/src/main/java/app/dto/UserDTO.java
@@ -1,4 +1,15 @@
 package app.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class UserDTO {
+    private UUID id;
+    private String username;
+    private String email;
 }
